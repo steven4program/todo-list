1cf3952a025bb33f786c056601d614c0822f8177
steven4program <stevenchang421@gmail.com>
2021-07-13 23:17:04 +0800

    refactor: modify express routes

---
+++ routes/modules/home.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../../models/todo');
+
+router.get('/', (req, res) => {
+  // get all todo data
+  Todo.find()
+    .lean()
+    .sort({ name: 'asc' })
+    .then((todos) => res.render('index', { todos }))
+    .catch((error) => console.error(error));
+});
+
+module.exports = router;
