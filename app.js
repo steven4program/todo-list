58ecdde16cac3d9852bb7ce7a71d2af62459eb21
steven4program <stevenchang421@gmail.com>
2021-07-14 22:39:32 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
-const Todo = require('./models/todo');
-
 const routes = require('./routes');
+require('./config/mongoose');
+
 const app = express();
-
-mongoose.connect('mongodb://localhost/todo-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-
-db.on('error', () => {
-  console.log('mongodb error!');
-});
-
-db.once('open', () => {
-  console.log('mongodb connected!');
-});
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
